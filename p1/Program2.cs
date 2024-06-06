fe78ca225a007ff06d9eb6542d4d2defb029374b
esanket <sanketvekariya237@gmail.com>
2024-06-06 11:11:31 +0530

    work on dotnet

---
+++ p1/Program2.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Program2
+{
+    public class Program2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name here");
+            string inp = Console.ReadLine();
+            Console.WriteLine(inp);
+            Console.ReadLine();
+        }
+    }
+}
+
+//take input from  user
