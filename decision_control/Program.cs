6322bd5b261cc7970d189049e85c0767bf87a6a3
esanket <sanketvekariya237@gmail.com>
2024-06-11 15:21:15 +0530

    c sharp methods

---
+++ decision_control/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Program{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter your age..!");
+            String age1 =Console.ReadLine();
+            int age = Convert.ToInt32(age1);
+
+            // if else else if ladder
+
+            if(age<18){
+                Console.WriteLine("you are not eligible");
+            }
+            else if(age>=18 && age !<80){
+                Console.WriteLine("you can drive on national highway");
+            }
+            else if(age>80){
+                Console.WriteLine("you can not drive on NH");
+            }
+            else{
+                Console.WriteLine("please provide vaalid input / you can not drive");
+            }
+
+            // switch case
+
+            Console.WriteLine("enter number");
+            string num = Console.ReadLine();
+            int number = Convert.ToInt32(num);
+            switch(number){
+                case 1:
+                Console.WriteLine("monday");
+                break;
+                case 2:
+                Console.WriteLine("tuesday");
+                break;
+                case 3:
+                Console.WriteLine("wenesday");
+                break;
+                case 4:
+                Console.WriteLine("thurday");
+                break;
+                case 5:
+                Console.WriteLine("friday");
+                break;
+                case 6:
+                Console.WriteLine("saturday");
+                break;
+                case 7:
+                Console.WriteLine("sunday");
+                break;    
+                default :
+                Console.WriteLine("you  have  enterd  invalid  input..");
+                break;
+    
+            }
+              
+            //   Loop - while dowhile for 
+
+            int i=0;
+            // while(i<10){
+            //     Console.WriteLine(i);
+            //     i++;
+            // }
+
+            int j=0;
+            do{
+                Console.WriteLine(j+1);
+            }while(j>100);
+
+            for(int k=0; k<10; k++){
+                Console.WriteLine(k);
+            }
+
+            Console.ReadLine();
+        }
+    }
+ }
---
+++ method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+namespace hello{
+    public class Program{
+
+        static int average(int a , int b, int c){
+            return (a+b+c)/3;
+        }
+        static int average(int a , int b){
+            return (a+b)/2;
+        }
+
+        // method overloading same name of methods but different arguments is called methodoverloading
+        public static void Main(string[] args)
+        {
+            // methods/..
+            String aa = Console.ReadLine();
+            String bb = Console.ReadLine();
+            String cc = Console.ReadLine();
+            int a = Convert.ToInt32(aa);
+            int b = Convert.ToInt32(bb);
+            int c = Convert.ToInt32(cc);
+
+            double av = average(a,b,c);
+
+            int d = 32;
+            int e = 32;
+            average(d,e);
+            Console.WriteLine(av);
+            Console.ReadLine();
+        }
+    }
+}
